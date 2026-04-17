d97cc48564250baaedc9b599c4413cf4156cbca1
tranhoc22102005-max <tranhoc22102005@gmail.com>
2026-04-17 21:03:06 +0700

    Update urls.py

--- urls.py
+++ urls.py
@@ -3,6 +3,7 @@ from . import views
 
 app_name = 'onlinecourse'
 urlpatterns = [
+    # Các path cũ...
     path('<int:course_id>/submit/', views.submit, name='submit'),
-    path('<int:course_id>/exam_result/', views.show_exam_result, name='show_exam_result'),
+    path('<int:course_id>/submission/<int:submission_id>/result/', views.show_exam_result, name='show_exam_result'),
 ]
